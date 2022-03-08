13e41bafc7d0be7d80fb57b92907f89ecc49d3ef
Arvind Nohar <arvind@mailbox.org>
2022-03-08 13:15:57 +0100

    add .config

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/sh
+
+#dir="$HOME/.config/polybar"
+
+#if type "xrandr"; then
+#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#    MONITOR=$m polybar --reload "$dir/config.ini" &
+#  done
+#else
+#  polybar --reload example &
+#fi
+
+#launch_bar() {
+# killall polybar
+#  while pgrep polybar; do killall polybar; done
+#  polybar -q main -c "$dir/config.ini"
+#}
+
+# launch_bar
+
---
+++ .config/polybar/launch2.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+
+killall -q polybar
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload side &
+fi
